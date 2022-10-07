fc81329d97e932aa648c1f71e5dba57c1a03a730
Dahan Schuster <dan.plschuster@gmail.com>
2022-10-07 10:51:54 -0300

    New tools to the list

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -23,7 +23,7 @@ echo "Upgrading packages"
 sudo apt full-upgrade -y
 echo
 
-UTILS="vim gedit curl git vlc pavucontrol alacarte gnome-tweak-tool net-tools xbacklight playerctl pactl"
+UTILS="vim gedit curl git vlc pavucontrol alacarte gnome-tweak-tool net-tools xbacklight playerctl pactl keepassxc rofi scrot gpicview redshift-gtk flameshot"
 echo "Installing some system utilities ($UTILS)"
 echo $UTILS | xargs sudo apt install -y
 echo
