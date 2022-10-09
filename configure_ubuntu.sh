ddb98f538235ebb629c581fce19746d537765487
Dahan Schuster <dan.plschuster@gmail.com>
2022-10-08 23:04:04 -0300

    Install i3wm; remove pactl

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -23,7 +23,7 @@ echo "Upgrading packages"
 sudo apt full-upgrade -y
 echo
 
-UTILS="vim gedit curl git vlc pavucontrol alacarte gnome-tweak-tool net-tools xbacklight playerctl pactl keepassxc rofi scrot gpicview redshift-gtk flameshot"
+UTILS="vim gedit curl git vlc pavucontrol alacarte gnome-tweak-tool net-tools xbacklight playerctl keepassxc rofi scrot gpicview redshift-gtk flameshot i3"
 echo "Installing some system utilities ($UTILS)"
 echo $UTILS | xargs sudo apt install -y
 echo
